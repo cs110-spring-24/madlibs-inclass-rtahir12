654ecdd2a71abeef5e4dc563135f3df27f84752e
raiyantahir <raiyantahir212@gmail.com>
2024-02-07 17:53:12 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,73 @@
-# TODO Create your inputs, variables, and add your story here!
+# Input 1
+noun1 = input("cat: ")
+adjective1 = input("curious: ")
+verb1 = input("jumped: ")
+
+# Input 2
+noun2 = input("book: ")
+adjective2 = input("old: ")
+verb2 = input("read: ")
+
+# Input 3
+noun3 = input("beach: ")
+adjective3 = input("sunny: ")
+verb3 = input("stretched: ")
+
+# Input 4
+noun4 = input("mountain: ")
+adjective4 = input("majestic: ")
+verb4 = input(" towered: ")
+
+# Input 5
+noun5 = input("bird: ")
+adjective5 = input("colorful: ")
+verb5 = input("sang: ")
+
+# Input 6
+noun6 = input("river: ")
+adjective6 = input("sparkling: ")
+verb6 = input("meandered: ")
+
+# Input 7
+noun7 = input("car: ")
+adjective7 = input("fast: ")
+verb7 = input("raced: ")
+
+# Input 8
+noun8 = input("flower: ")
+adjective8 = input("fragrant: ")
+verb8 = input("bloomed: ")
+
+# Input 9
+noun9 = input("boat: ")
+adjective9 = input("wooden: ")
+verb9 = input("floated: ")
+
+# Input 10
+noun10 = input("star: ")
+adjective10 = input("twinkling: ")
+verb10 = input("shone: ")
+
+# Create and print the stories using the user inputs
+story1 = f"The {curious} {cat} {jumped} through the forest."
+story2 = f"The {old} {book} {read} across the field."
+story3 = f"The {sunny} {beach} {stretched} on the mountain."
+story4 = f"The {majestic} {mountain} {towered} in the cave."
+story5 = f"The {colorful} {bird} {sang} under the sea."
+story6 = f"The {sparkling} {river} {meandered} in the city."
+story7 = f"The {fast} {car} {raced} on the beach."
+story8 = f"The {fragrant} {flower} {bloomed} in the desert."
+story9 = f"The {wooden} {boat} {floated} in the sky."
+story10 = f"The {twinkling} {star} {shone} through the canyon."
+
+print("Here are your stories:")
+print(story1)
+print(story2)
+print(story3)
+print(story4)
+print(story5)
+print(story6)
+print(story7)
+print(story8)
+print(story9)
+print(story10)
